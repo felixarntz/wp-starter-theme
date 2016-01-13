e09922c100ecafd0ea28d0d1f4778b15708d1cb3
Felix Arntz <felix-arntz@t-online.de>
2016-01-13 02:48:53 +0100

    improved navigation menu class, added some initial styles, added styles for social links menu

--- inc/WPStarterTheme/Base/Util/NavMenu.php
+++ inc/WPStarterTheme/Base/Util/NavMenu.php
@@ -7,29 +7,38 @@
 namespace WPStarterTheme\Base\Util;
 
 final class NavMenu extends \Walker_Nav_Menu {
-	public function check_current( $classes ) {
-		return preg_match( '/(current[-_])|active|dropdown/', $classes );
-	}
+	private static $li_class = '';
+	private static $a_class = '';
 
 	public function start_lvl( &$output, $depth = 0, $args = array() ) {
-		$output .= '<ul class="dropdown-menu">';
+		$html = '';
+
+		parent::start_lvl( $html, $depth, $args );
+
+		$html = str_replace( 'sub-menu', 'dropdown-menu', $html );
+
+		$output .= $html;
 	}
 
 	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-		$item_html = '';
+		$html = '';
 
-		parent::start_el( $item_html, $item, $depth, $args );
+		parent::start_el( $html, $item, $depth, $args );
 
 		if ( $item->is_dropdown && ( $depth === 0 ) ) {
-			$item_html = str_replace( '<a', '<a class="dropdown-toggle" data-toggle="dropdown" data-target="#"', $item_html );
-			$item_html = str_replace( '</a>', ' <b class="caret"></b></a>', $item_html );
-		} elseif( stristr( $item_html, 'li class="divider' ) ) {
-			$item_html = preg_replace( '/<a[^>]*>.*?<\/a>/iU', '', $item_html );
-		} elseif( stristr( $item_html, 'li class="dropdown-header' ) ) {
-			$item_html = preg_replace( '/<a[^>]*>(.*)<\/a>/iU', '$1', $item_html );
+			if ( false !== strpos( $html, '<a class="' ) ) {
+				$html = str_replace( '<a class="', '<a data-toggle="dropdown" data-target="#" class="dropdown-toggle ', $html );
+			} else {
+				$html = str_replace( '<a', '<a class="dropdown-toggle" data-toggle="dropdown" data-target="#"', $html );
+			}
+			$html = str_replace( '</a>', ' <b class="caret"></b></a>', $html );
+		} elseif( stristr( $html, 'li class="divider' ) ) {
+			$html = preg_replace( '/<a[^>]*>.*?<\/a>/iU', '', $html );
+		} elseif( stristr( $html, 'li class="dropdown-header' ) ) {
+			$html = preg_replace( '/<a[^>]*>(.*)<\/a>/iU', '$1', $html );
 		}
 
-		$output .= $item_html;
+		$output .= $html;
 	}
 
 	public function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
@@ -50,28 +59,70 @@ final class NavMenu extends \Walker_Nav_Menu {
 			$args['items_wrap'] = '<ul class="%2$s">%3$s</ul>';
 		}
 
-		if ( isset( $args['theme_location'] ) && 'primary' === $args['theme_location'] ) {
-			$args['depth'] = 2;
-			$args['walker'] = new self();
+		if ( isset( $args['menu_class'] ) ) {
+			$menu_classes = explode( ' ', $args['menu_class'] );
+
+			if ( in_array( 'list-inline', $menu_classes, true ) ) {
+				$args['li_class'] = 'list-inline-item';
+				$args['depth'] = 1;
+			} elseif ( in_array( 'navbar-nav', $menu_classes, true ) ) {
+				$args['li_class'] = 'nav-item';
+				$args['a_class'] = 'nav-link';
+			}
+
+			if ( in_array( 'social-menu', $menu_classes, true ) ) {
+				$args['link_before'] = '<span class="sr-only">';
+				$args['link_after'] = '</span>';
+			}
 		}
 
-		return \wp_nav_menu( $args );
+		$args['walker'] = new self();
+
+		if ( isset( $args['li_class'] ) ) {
+			self::$li_class = $args['li_class'];
+			unset( $args['li_class'] );
+		}
+		if ( isset( $args['a_class'] ) ) {
+			self::$a_class = $args['a_class'];
+			unset( $args['a_class'] );
+		}
+
+		$output = \wp_nav_menu( $args );
+
+		self::$li_class = '';
+		self::$a_class = '';
+
+		return $output;
 	}
 
 	public static function init() {
-		add_filter( 'nav_menu_css_class', array( __CLASS__, 'fix_css_classes' ), 10, 2 );
+		add_filter( 'nav_menu_css_class', array( __CLASS__, 'fix_css_classes' ), 10, 4 );
+		add_filter( 'nav_menu_link_attributes', array( __CLASS__, 'fix_link_attributes' ), 10, 4 );
 		add_filter( 'nav_menu_item_id', '__return_null' );
 	}
 
-	public static function fix_css_classes( $classes, $item ) {
+	public static function fix_css_classes( $classes, $item, $args, $depth ) {
 		$slug = sanitize_title( $item->title );
 
 		$classes = preg_replace( '/(current(-menu-|[-_]page[-_])(item|parent|ancestor))/', 'active', $classes );
 		$classes = preg_replace( '/^((menu|page)[-_\w+]+)+/', '', $classes );
 		$classes[] = 'menu-' . $slug;
+
+		if ( ! empty( self::$li_class ) ) {
+			$classes[] = self::$li_class;
+		}
+
 		$classes = array_unique( $classes );
 
 		return array_filter( $classes, array( __CLASS__, 'is_class_valid' ) );
+	}
+
+	public static function fix_link_attributes( $atts, $item, $args, $depth ) {
+		if ( ! empty( self::$a_class ) ) {
+			$atts = array_merge( array( 'class' => self::$a_class ), $atts );
+		}
+
+		return $atts;
 	}
 
 	public static function is_class_valid( $class ) {
