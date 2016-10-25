35fff7e90c1982c0e4488a555e7a33cba6afd06d
Felix Arntz <felix-arntz@t-online.de>
2016-10-25 11:19:49 +0200

    add theme support for Jetpack Infinite Scroll and Frontkit

--- inc/WPStarterTheme/Base/Theme.php
+++ inc/WPStarterTheme/Base/Theme.php
@@ -65,11 +65,13 @@ final class Theme {
 		add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
 		add_theme_support( 'post-thumbnails' );
 
+		/* Support for plugins. */
 		add_theme_support( 'infinite-scroll', array(
 			'container'      => 'posts-list',
 			'footer'         => false,
 			'footer_widgets' => false,
 			'wrapper'        => false,
+			'render'         => array( $this->partials(), 'render_loop' ),
 		) );
 		add_theme_support( 'frontkit', array(
 			'title'   => '.single-post .post-title',
