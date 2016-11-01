5991f6eb01fd3062508a2736c492eef47d18792f
Felix Arntz <felix-arntz@t-online.de>
2016-11-01 21:17:49 +0100

    use proper charset setting

--- header.php
+++ header.php
@@ -10,7 +10,7 @@ namespace WPStarterTheme;
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
-		<meta charset="utf-8">
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<?php wp_head(); ?>
