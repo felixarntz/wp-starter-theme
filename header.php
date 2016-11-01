3c856231ece783aeb200b7049cb345b7ed870af6
Felix Arntz <felix-arntz@t-online.de>
2016-11-01 21:18:44 +0100

    add profile link

--- header.php
+++ header.php
@@ -6,13 +6,13 @@
 
 namespace WPStarterTheme;
 
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<?php wp_head(); ?>
 	</head>
 
