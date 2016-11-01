089798850c5354d966752929de99074e434d13a8
Felix Arntz <felix-arntz@t-online.de>
2016-11-01 21:11:30 +0100

    add basic support for EDD buttons

--- inc/WPStarterTheme/Base/PluginCompat.php
+++ inc/WPStarterTheme/Base/PluginCompat.php
@@ -19,9 +19,19 @@ final class PluginCompat extends ThemeUtilityBase {
 	 * @access public
 	 */
 	public function run() {
+		/* Jetpack */
 		add_action( 'after_setup_theme', array( $this, 'jetpack_theme_support' ) );
+
+		/* Frontkit */
 		add_action( 'after_setup_theme', array( $this, 'frontkit_theme_support' ) );
 
+		/* Easy Digital Downloads */
+		add_filter( 'shortcode_atts_purchase_link', array( $this, 'edd_button_args' ), 10, 3 );
+		add_filter( 'edd_purchase_link_defaults', array( $this, 'edd_button_args' ) );
+		add_filter( 'edd_checkout_button_next',     array( $this, 'edd_button' ) );
+		add_filter( 'edd_checkout_button_purchase', array( $this, 'edd_button' ) );
+
+		/* Give WP */
 		add_filter( 'give_display_checkout_button', array( $this, 'give_button' ) );
 		add_filter( 'give_checkout_button_purchase', array( $this, 'give_button' ) );
 	}
@@ -58,7 +68,45 @@ final class PluginCompat extends ThemeUtilityBase {
 	}
 
 	/**
-	 * Adjusts button classes for the Give WP plugin.
+	 * Adjusts purchase button classes for Easy Digital Downloads.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @internal
+	 *
+	 * @param array $args     Purchase link or download shortcode arguments.
+	 * @param array $defaults Optional. Download shortcode defaults.
+	 * @param array $atts     Optional. Download shortcode attributes.
+	 * @return array The modified arguments.
+	 */
+	public function edd_button_args( $args, $defaults = null, $atts = null ) {
+		if ( is_array( $defaults ) && is_array( $atts ) ) {
+			if ( ! isset( $atts['class'] ) ) {
+				$args['class'] = 'edd-submit btn btn-primary';
+			}
+		} else {
+			$args['class'] = 'edd-submit btn btn-primary';
+		}
+
+		return $args;
+	}
+
+	/**
+	 * Adjusts checkout button classes for Easy Digital Downloads.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @internal
+	 *
+	 * @param string $output Button output.
+	 * @return string Modified button output.
+	 */
+	public function edd_button( $output ) {
+		return str_replace( 'class="edd-submit ', 'class="edd-submit btn btn-primary ', $output );
+	}
+
+	/**
+	 * Adjusts button classes for Give WP.
 	 *
 	 * @since 1.0.0
 	 * @access public
